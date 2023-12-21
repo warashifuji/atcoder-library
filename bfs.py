cf140456e9b0cde9450637dbbeb3fdd6788d4309
Fujiwara Shuhei <fujiwara@FujiwaranoMacBook-Air.local>
2023-12-22 01:58:23 +0900

    bfs追加

---
+++ bfs.py
@@ -0,0 +1,22 @@
+from collections import deque
+def bfs(i):
+    deq = deque()
+    dist = [-1] * N
+    deq.append(i)
+    dist[i] = 0
+    while deq:
+        v = deq.popleft()
+        for e in edge[v]:
+            if dist[e] != -1:
+                continue
+            dist[e] = dist[v] + 1
+            deq.append(e)
+    return dist
+
+N, M = map(int, input().split())
+edge = [[] for _ in range(N)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    a -= 1
+    b -= 1
+    edge[a].append(b)
